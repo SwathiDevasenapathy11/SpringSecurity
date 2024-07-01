d683c01d363b60e7a7c63a87faff92497c9a0104
Devaswathi <swathidevasenapathy@gmail.com>
2024-07-01 10:40:59 +0530

    Crud API for users (registered)

--- auth/src/main/java/com/auth/service/UserService.java
+++ auth/src/main/java/com/auth/service/UserService.java
@@ -1,5 +1,8 @@
 package com.auth.service;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -23,6 +26,27 @@ public class UserService {
 		user.setPassword(passwordEncoder.encode(user.getPassword()));
 		user.setConfirmPassword(passwordEncoder.encode(user.getConfirmPassword()));
 		return userRepository.save(user);
+	}
+	
+	public List<User> get(){
+		return userRepository.findAll();
+	}
+	
+	public User update(User user) {
+		Optional<User> updateUser = userRepository.findById(user.getId());
+		if(updateUser.isPresent()) {
+			User existingUser = updateUser.get();
+			existingUser.setEmail(user.getEmail());
+			existingUser.setUsername(user.getUsername());
+			return userRepository.save(existingUser);
+		} else {
+			throw new RuntimeException("No data found with that id");
+		}
+//		return null;
+	}
+	
+	public void delete(Long id) {
+		 userRepository.deleteById(id);
 	}
 
 }
